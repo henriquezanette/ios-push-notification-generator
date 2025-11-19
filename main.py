53e59e1d2f1d476590272733935789681e981636
henriquezanette <henriquezanette1@gmail.com>
2025-11-19 18:41:03 -0300

    feat: added functions for bundle identifier and devices

--- main.py
+++ main.py
@@ -29,3 +29,34 @@ def create_apns_payload(title, body, action, bundle):
     }
 
     return json.dumps(apns_payload)
+
+
+def get_simulator_devices(available_devices):
+    result = subprocess.run(
+        ["xcrun", "simctl", "list", "devices", "booted", "--json"],
+        capture_output=True,
+        text=True,
+    )
+    devices = json.loads(result.stdout)
+
+    for device_list in devices["devices"].items():
+        for device in device_list:
+            if "state" in device:
+                model = device["name"]
+                udid = device["udid"]
+                if model.startswith("iPhone"):
+                    print(f"- Model: {model}, Device Token: {udid}")
+                    available_devices.append((model, udid))
+
+
+def get_bundle_identifiers(bundle_choices, device_udid):
+    output = subprocess.check_output(["xcrun", "simctl", "listapps", device_udid])
+    output_options = output.decode("utf-8").splitlines()
+
+    for line in output_options:
+        if "CFBundleIdentifier" in line:
+            start_index = line.find('"') + 1
+            end_index = line.find('"')
+            cf_bundle_identifier = line[start_index:end_index]
+
+            bundle_choices.append(cf_bundle_identifier)
